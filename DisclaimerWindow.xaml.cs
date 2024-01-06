fe81ef1100a2e5ab8eeb7a12ce848d552080faab
Jestzer <James@Buonopane.email>
2024-01-06 00:17:47 -0500

    Working on disclaimer window.

---
+++ DisclaimerWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Force.Halo.Checkpoints
+{
+    public partial class DisclaimerWindow : Window
+    {
+        public DisclaimerWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Implement window dragging with the mouse.
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
